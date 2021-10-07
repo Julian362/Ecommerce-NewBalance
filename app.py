5d5d006557cdb5cbf86162b19d0402c4a1aa8507
Julian362 <54406277+Julian362@users.noreply.github.com>
2021-10-07 15:53:09 -0500

    se creo login.html, login.css, y se actualiza index.html

--- app.py
+++ app.py
@@ -4,4 +4,8 @@ app = Flask(__name__)
 
 @app.route('/index')
 def hola_mundo():
-    return render_template('index.html') 
+    return render_template('index.html')
+
+@app.route('/login/')
+def login():
+    return render_template('login.html')
