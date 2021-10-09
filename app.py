9eaa534e31d1ba0eedb905001abbeb4c20c5efec
Racastano1 <raulkastano1@gmail.com>
2021-10-08 21:01:24 -0500

    Lista de productos - vista preliminar

--- app.py
+++ app.py
@@ -18,6 +18,16 @@ def registro():
 def gestioncomentario():
     return render_template('gestion_comentario.html')
 
+"""Ruta para llamar a los productos de hombre"""
+@app.route('/productos_hombre/')
+def lista_de_productos_hombre():
+    return render_template('lista_de_productos.html')
+
+"""Ruta para llamar a los productos de mujer"""
+@app.route('/productos_mujer/')
+def lista_de_productos_mujer():
+    return render_template('lista_de_productos.html')
+
 @app.route('/')
 def index():
     return render_template('index.html')
