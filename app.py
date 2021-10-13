78e824fbcd10d26c9baf215b96cef3fa458f22e8
DavHD09 <davidhdz99@hotmail.com>
2021-10-12 21:06:06 -0500

    actualización

--- app.py
+++ app.py
@@ -24,10 +24,6 @@ def login():
 def registro():
     return render_template('registro.html')
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
 @app.route('/producto/')
 def productoind():
     return render_template('ProductoIndividual.html')
