505d2703d43de4479c5a545336efd0d9081da3ad
DavHD09 <davidhdz99@hotmail.com>
2021-10-12 23:49:14 -0500

    Actualización visual productos y comentarios, y visual inputs

--- static/Js/Productoindividual.js
+++ static/Js/Productoindividual.js
@@ -4,7 +4,7 @@ function abrir1(){
     document.getElementById("pri").style.background="#cccccc";
     document.getElementById("pri").style.color="red";
     document.getElementById("sda").style.background="none";
-    document.getElementById("sda").style.color="#007bff";
+    document.getElementById("sda").style.color="#242942";
 }
 
 
@@ -12,7 +12,7 @@ function abrir2(){
     document.getElementById("tab2").style.display="block";
     document.getElementById("tab1").style.display="none";
     document.getElementById("pri").style.background="none";
-    document.getElementById("pri").style.color="#007bff";
+    document.getElementById("pri").style.color="#242942";
     document.getElementById("sda").style.background="#cccccc";
     document.getElementById("sda").style.color="red";
 }
