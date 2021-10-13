c297b0396b0ae699b13d9eb2a73a4212d1e22cd6
Racastano1 <raulkastano1@gmail.com>
2021-10-12 21:04:52 -0500

    Edición de productos y todos los comentarios

--- app.py
+++ app.py
@@ -119,12 +119,12 @@ def block_usuario(nickname, estado):
     return render_template('administrador.html',error="No se pudo bloquear al usuario",lista_usuarios=usuario.listado())
 
 """Ruta para llamar a los productos de hombre"""
-@app.route('/productos_hombre/')
+@app.route('/productos/hombre/')
 def lista_de_productos_hombre():
     return render_template('productos_hombre.html')
 
 """Ruta para llamar a los productos de mujer"""
-@app.route('/productos_mujer/')
+@app.route('/productos/mujer/')
 def lista_de_productos_mujer():
     return render_template('productos_mujer.html')
 
@@ -143,7 +143,7 @@ def superadministrador():
     return render_template('superadministrador.html')
 
 """Ruta para todos los comentarios de un producto"""
-@app.route('/todos_los_comentario/')
+@app.route('/comentarios/')
 def todos_los_comentarios():
     return render_template('todos_los_comentarios.html')
 
