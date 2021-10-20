c8d30069e3af51a788e10d2afc5d16d201dadfcc
Julian362 <54406277+Julian362@users.noreply.github.com>
2021-10-20 07:17:09 -0500

    actualización comandos sql administrador

--- models.py
+++ models.py
@@ -1,19 +1,19 @@
 import db
 
 #Clase usuario, para la respectiva pantalla.
-class usuario(): 
+class usuario():
 
     estado = 'F'
     nickname = ''
     nombre = ''
     apellidos = ''
     documento = ''
-    celular = ''
+    telefono = ''
     sexo = ''
     correo = ''
 
     #Se establece el método constructor
-    def __init__(self, p_nickname, p_estado, p_nombre, p_apellidos, p_documento, p_celular, p_sexo, p_correo) -> None:
+    def __init__(self, p_nickname, p_estado, p_nombre, p_apellidos, p_documento, p_telefono, p_sexo, p_correo) -> None:
         self.estado = p_estado
         self.nickname = p_nickname
         self.nombre = p_nombre
@@ -21,24 +21,24 @@ class usuario():
         self.documento = p_documento
         self.sexo = p_sexo
         self.correo = p_correo
-        self.celular = p_celular
+        self.telefono = p_telefono
 
     #Función para cargar los datos
     @classmethod
-    def cargar(cls, p_nickname):
-        sql = 'SELECT * FROM usuario Where nickname = ?;'
-        obj = db.ejecutar_select(sql,[ p_nickname ])
+    def cargar(cls, p_documento):
+        sql = 'SELECT DISTINCT persona.* FROM persona inner join rol on persona.tipo_rol = "user" and persona.documento="?";'
+        obj = db.ejecutar_select(sql,[ p_documento ])
         if obj:
             if len(obj)>0:
-                return cls(obj[0]["nickname"],obj[0]["estado"],obj[0]["nombre"],obj[0]["apellidos"],obj[0]["documento"],obj[0]["celular"],obj[0]["sexo"],obj[0]["correo"])
+                return cls(obj[0]["nickname"],obj[0]["estado"],obj[0]["nombre"],obj[0]["apellidos"],obj[0]["documento"],obj[0]["telefono"],obj[0]["sexo"],obj[0]["correo"])
 
         return None
 
     #Función para eliminar
     @classmethod
-    def delete(cls, p_nickname):
-        sql = 'DELETE FROM usuario Where nickname = ?;'
-        obj = db.ejecutar_insert(sql,[ p_nickname ])
+    def delete(cls, p_documento):
+        sql = 'DELETE FROM persona inner join rol on persona.tipo_rol = "user" and persona.documento="?";'
+        obj = db.ejecutar_insert(sql,[ p_documento ])
         if obj:
             if obj>0:
                 return "Borrado corectamente el usuario "
@@ -47,12 +47,12 @@ class usuario():
 
     #Función para el bloquear o desbloquear al usuario
     @classmethod
-    def block(cls, p_nickname, p_estado):
+    def block(cls, p_documento, p_estado):
         if p_estado == "T":
-            sql = 'UPDATE usuario set estado = "F" Where nickname = ?;'
+            sql = 'UPDATE persona set estado = "F" Where documento = ?;'
         elif p_estado == "F":
-            sql = 'UPDATE usuario set estado = "T" Where nickname = ?;'
-        obj = db.ejecutar_insert(sql,[ p_nickname ])
+            sql = 'UPDATE persona set estado = "T" Where documento = ?;'
+        obj = db.ejecutar_insert(sql,[ p_documento ])
         if obj:
             if obj > 0:
                 return True
@@ -61,17 +61,17 @@ class usuario():
 
     #Función para editar los datos de usuario
     @classmethod
-    def editar(cls,nombre, apellidos, correo, documento, celular, nickname, sexo):
-        sql="UPDATE usuario set nombre = ?,apellidos = ?, correo= ? ,documento= ? , celular= ?, sexo= ? where nickname= ?"
-        obj = db.ejecutar_insert(sql,[nombre, apellidos, correo, documento, celular,  sexo, nickname])
+    def editar(cls,nombre, apellidos, correo, documento, telefono, nickname, sexo):
+        sql="UPDATE persona set nombre = ?,apellidos = ?, correo= ? ,documento= ? , telefono= ?, sexo= ? where documento= ?"
+        obj = db.ejecutar_insert(sql,[nombre, apellidos, correo, documento, telefono,  sexo, nickname])
         if obj:
             if obj > 0:
                 return True
 
     #Función para crear al usuario
-    def crear(cls,nombre, apellidos, correo, documento, celular, nickname, sexo):
-        sql="INSERT INTO usuario (estado,nickname,nombre,apellidos,documento,sexo,correo,celular) VALUES (?,?,?,?,?,?,?,?);"
-        obj = db.ejecutar_insert(sql,["T",nickname,nombre, apellidos, documento,sexo, correo, celular])
+    def crear(cls,nombre, apellidos, correo, documento, telefono, nickname, sexo):
+        sql="INSERT INTO persona (estado,nickname,nombre,apellidos,documento,sexo,correo,telefono) VALUES (?,?,?,?,?,?,?,?);"
+        obj = db.ejecutar_insert(sql,["T",nickname,nombre, apellidos, documento,sexo, correo, telefono])
         if obj:
             if obj > 0:
                 return True
@@ -81,5 +81,5 @@ class usuario():
     #Función para obtener el listado de los usuarios
     @staticmethod
     def listado():
-        sql = "SELECT * FROM usuario ORDER BY nickname;"
+        sql = 'SELECT persona.* FROM persona inner join rol on persona.tipo_rol = "user" ORDER BY documento;'
         return db.ejecutar_select(sql, None)
