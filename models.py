c46a2ec659f21f178785d7862b44c4552505985e
Julian362 <54406277+Julian362@users.noreply.github.com>
2021-10-22 11:04:50 -0500

    actualización imagenes, y productos_hombre.html

--- models.py
+++ models.py
@@ -177,6 +177,10 @@ class producto():
 
     @staticmethod
     def listado():
-        sql = 'select producto.estado, producto.nombre, producto.precio, inventario.referencia_producto as referencia, inventario.cantidad, inventario.talla from producto inner join inventario on inventario.referencia_producto=producto.referencia order by nombre asc;'
+        sql = 'select producto.estado, producto.nombre, producto.precio, inventario.referencia_producto as referencia, inventario.cantidad, inventario.talla  from producto inner join inventario on inventario.referencia_producto=producto.referencia  order by nombre asc;'
+        return db.ejecutar_select(sql, None)
+
+    def listado_referencia():
+        sql = 'select producto.estado, producto.nombre, producto.precio, inventario.referencia_producto as referencia, inventario.cantidad, inventario.talla  from producto inner join inventario on inventario.referencia_producto=producto.referencia  group by referencia  order by nombre asc;'
         return db.ejecutar_select(sql, None)
 
